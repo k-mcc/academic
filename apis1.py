dc60ed8124d36b8e74c507a672c59e3d7026e3e8
k-mcc <67031674+k-mcc@users.noreply.github.com>
2020-12-07 12:08:15 -0500

    Consolidate apis1.py

--- apis1.py
+++ apis1.py
@@ -2,5 +2,45 @@
 # 12.5.20
 
 import requests
-response = requests.get("http://api.open-notify.org/iss-now.json")
-print(response.status_code)
+import json
+import random
+
+def json_extract(obj, key):
+    """Recursively fetch values from nested JSON."""
+    arr = []
+
+    def extract(obj, arr, key):
+        """Recursively search for values of key in JSON tree."""
+        if isinstance(obj, dict):
+            for k, v in obj.items():
+                if isinstance(v, (dict, list)):
+                    extract(v, arr, key)
+                elif k == key:
+                    arr.append(v)
+        elif isinstance(obj, list):
+            for item in obj:
+                extract(item, arr, key)
+        return arr
+
+    values = extract(obj, arr, key)
+    return values
+
+def promptWords(prompt):
+    input = raw_input("{}:\t".format(prompt))
+    return input
+
+def getRhyme(word):
+    response = requests.get('https://rhymebrain.com/talk?function=getRhymes&word={}'.format(str(word)))
+    response_json = response.json()
+    rhymingWords = json_extract(response_json, "word")
+    return rhymingWords
+
+
+word1 = promptWords("Please enter your first name")
+
+words = getRhyme(word1)
+
+for i in range(10): #len(rhymingWords)
+    print(words[i])
+
+print("Word: {}".format(random.choice(words)))
